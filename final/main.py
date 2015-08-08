2f8c09c6dd07d55a0fdc6069222e037eee5ed931
Suvanjan Mukherjee <suvanjanmukherjee@gmail.com>
2015-08-07 23:51:38 -0400

    Add code for the final wiki project. Mostly working at this point

---
+++ final/main.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import time
+import logging
+from utils import *
+
+
+class LoginHandler(Handler):
+	def render_login(self,
+		username="",
+		uerror="",
+		perror=""):
+		return self.render('login.html',
+			username=username,
+			uerror=uerror,
+			perror=perror)
+
+	def get(self):
+		return self.render_login()
+
+	def post(self):
+		username = self.request.get('username')
+		password = self.request.get('password')
+		s_username = sanitize(username)
+
+		uerror = ""
+		perror = ""
+		error = False
+		if not valid_username(username):
+			uerror = "That's not a valid username."
+			error = True
+		if not valid_password(password):
+			perror = "That wasn't a valid password."
+			error = True
+
+		user = None
+		if not uerror:
+			user = get_user(s_username)
+			if not user:
+				uerror = "That user doesn't exist!"
+				error = True
+			elif not check_user_info(s_username, password, user.password):
+				perror = "Invalid login"
+				error = True
+		if error:
+			return self.render_login(username=s_username,
+				uerror=uerror,
+				perror=perror)
+		set_user_cookie(self.response, user)
+		return self.redirect('/')
+
+class LogoutHandler(Handler):
+	def get(self):
+		unset_user_cookie(self.response)
+		return self.redirect('/')
+
+
+
+
+class SignupHandler(Handler):
+	def render_signup(self,
+		username="",
+		email="",
+		uerror="",
+		perror="",
+		pverror="",
+		eerror=""):
+		return self.render('signup.html',
+			username="",
+			email="",
+			uerror=uerror,
+			perror=perror,
+			pverror=pverror,
+			eerror=eerror)
+
+	def get(self):
+		return self.render_signup()
+
+	def post(self):
+		username = self.request.get('username')
+		password = self.request.get('password')
+		verify = self.request.get('verify')
+		email = self.request.get('email')
+
+		s_username = sanitize(username)
+		s_email = sanitize(email)
+
+		uerror = ""
+		perror = ""
+		pverror = ""
+		eerror = ""
+		error = False
+
+		if not valid_username(username):
+			uerror = "That's not a valid username."
+			error = True
+		if not valid_password(password):
+			perror = "That wasn't a valid password."
+			error = True
+		elif password != verify:
+			pverror = "Your passwords didn't match"
+			error = True
+		if email and not valid_email(email):
+			eerror = "That's not a valid email."
+			error = True
+		if error:
+			return self.render_signup(uerror=uerror,
+				perror=perror,
+				pverror=pverror,
+				eerror=eerror,
+				username=s_username,
+				email=s_email)
+
+		user = get_user(s_username)
+		if not user:
+			if not email:
+				s_email = None
+			user = User(name=s_username, 
+				password=hash_user_info(s_username, password),
+				email=s_email)
+			user.put()
+		else:
+			return self.render_signup(uerror="User already exists!",
+				username=s_username)
+		set_user_cookie(self.response, user)
+
+		return self.redirect('/')
+
+
+class MainHandler(Handler):
+
+	def get(self, title="front"):
+		logging.info("Wiki Title request " + title + ", user: " + str(self.logged_in_user))
+		wiki = Wiki.getEntryByTitle(title)
+		if wiki:
+			return self.render('index.html', 
+				logged_in_user=self.logged_in_user,
+				edit_link="/_edit/" + title,
+				content=wiki.content)
+		elif self.logged_in_user:
+			self.redirect('/_edit/' + title)
+		elif title == "front":
+			self.response.write('''
+Oops! Seems like our front page is messed up. Since we are a low budget site and don't have
+a group of highly trained monkeys like Google to fix this, this may take a while :(
+''')
+
+
+class EditHandler(Handler):
+
+	def get(self, title):
+		if not self.logged_in_user:
+			self.redirect('/')
+		logging.info("Wiki Title edit request " + title)
+		wiki = Wiki.getEntryByTitle(title)
+		content=""
+		if wiki:
+			content = wiki.content
+		return self.render('new.html', 
+			logged_in_user=self.logged_in_user,
+			content=content)
+
+	def post(self, title):
+		if not self.logged_in_user:
+			self.redirect('/')
+		content = self.request.get('content')
+		wiki = Wiki.getEntryByTitle(title)
+		if wiki:
+			logging.critical('TODO: Make this a part of transaction')
+			wiki.content = content
+			wiki.put()
+		else:
+			wiki = Wiki(title=title, content=content)
+			wiki.put()
+
+		if title == 'front':
+			title = ""
+
+		return self.redirect('/' + title)
+
+
+
+app = webapp2.WSGIApplication([
+	('/', MainHandler),
+	('/signup/?', SignupHandler),
+	('/login/?', LoginHandler),
+	('/logout/?', LogoutHandler),
+	('/_edit/' + PAGE_RE_STR, EditHandler),
+	('/' + PAGE_RE_STR, MainHandler),
+], debug=True)
---
+++ final/utils.py
@@ -0,0 +1,155 @@
+import webapp2
+import os
+import cgi
+from google.appengine.ext import db
+import jinja2
+import hashlib
+import hmac
+import string
+import random
+import datetime
+import re
+import time
+import logging
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader= jinja2.FileSystemLoader(template_dir),
+	autoescape= True)
+random.seed()
+
+USER_RE = re.compile(r'^[a-zA-Z0-9_-]{3,20}$')
+PASS_RE = re.compile(r'^.{3,20}$')
+EMAIL_RE = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+PAGE_RE_STR = r'([_0-9a-zA-Z]+)'
+
+SECRET = 'NA3kjq8K5R'
+SALT_CHARS = string.ascii_letters + string.digits + '@#$%!&*'
+
+def make_salt(slen=5):
+	salt = ''
+	for i in xrange(slen):
+		salt += random.choice(SALT_CHARS)
+	return salt
+
+def hash_user_info(username, password, salt=None):
+	if not salt:
+		salt = make_salt()
+	h = hashlib.sha256(username + password + salt).hexdigest()
+	#h = hmac.new(SECRET, password + salt, hashlib.sha256).hexdigest()
+	h += '|' + salt
+	return h
+
+def check_user_info(username, password, h):
+	salt = None
+	try:
+		salt = h.split('|')[1]
+	except IndexError:
+		return False
+	if h == hash_user_info(username, password, salt):
+		return True
+	return False
+
+
+def valid_username(username):
+	return USER_RE.match(username)
+
+def valid_password(password):
+	return PASS_RE.match(password)
+
+def valid_email(email):
+	return EMAIL_RE.match(email)
+
+def escape_html(s):
+	return cgi.escape(s, quote=True)
+
+def sanitize(s):
+	r = ''
+	for c in s:
+		r += escape_html(c)
+	return r
+
+def get_user(username):
+	q = db.GqlQuery("SELECT * from User WHERE name=:1", username)
+	return q.get()
+
+def hash_username_for_cookie(username):
+	h = hmac.new(SECRET, username, hashlib.sha256)
+	return h.hexdigest()
+
+def verify_user_cookie(user_cookie):
+	split_cookie = user_cookie.split('|')
+	if len(split_cookie) != 2:
+		print split_cookie
+		return False
+	username = None
+	hashed_username = None
+	try:
+		username = split_cookie[0]
+		hashed_username = split_cookie[1]
+	except IndexError:
+		return False
+	if hashed_username == hash_username_for_cookie(username):
+		return username
+	return
+
+
+def set_user_cookie(response, user):
+	response.set_cookie('user', 
+		value=user.name + '|' + hash_username_for_cookie(user.name), 
+		path='/', 
+		#domain='www.suvanjanmukherjee.me',
+		expires=datetime.datetime.now() + datetime.timedelta(days=1))
+	#username = str(user.name)
+	#cookie_value = username + '|' + hash_username_for_cookie(username)
+	#response.headers.add_header('Set-Cookie', 'user=%s; Path=/'%cookie_value)
+	return
+
+def unset_user_cookie(response):
+	response.delete_cookie('user')
+	return
+
+class Wiki(db.Model):
+	title = db.StringProperty(required=True)
+	content = db.TextProperty(required=True)
+	created = db.DateTimeProperty(auto_now_add=True)
+	updated = db.DateTimeProperty(auto_now=True)
+
+	@staticmethod
+	def getEntryByTitle(title):
+		wikis = list(Wiki.all().filter('title =', title))
+
+		if wikis:
+			if len(wikis) != 1:
+				logging.error("%d wikis with title '%s'"%(len(wikis), title))
+			else:
+				return wikis[0]
+		return
+
+class User(db.Model):
+	name = db.StringProperty(required=True)
+	password = db.StringProperty(required=True)
+	email = db.EmailProperty(required=False)
+	created = db.DateTimeProperty(auto_now_add=True)
+	updated = db.DateTimeProperty(auto_now=True)
+
+
+class Handler(webapp2.RequestHandler):
+	def dispatch(self):
+		user_cookie = self.request.cookies.get('user', '')
+		username = verify_user_cookie(user_cookie)
+		if username:
+			self.logged_in_user = username
+		else:
+			self.logged_in_user = None
+		super(Handler, self).dispatch()
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.response.write(self.render_str(template, **kw))
+
+
+
+
